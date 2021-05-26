c14619c96266c11c1eac62f24db5646ba85c1950
raveerocks <raveendra.tudangil@gmail.com>
2021-05-27 00:42:34 +0530

    :triangular_flag_on_post: LocalFactory added which hanles BrowserStack Local connection

---
+++ src/test/java/com/browserstack/examples/config/LocalFactory.java
@@ -0,0 +1,68 @@
+package com.browserstack.examples.config;
+
+import com.browserstack.local.Local;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import java.util.Map;
+
+
+public class LocalFactory {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocalFactory.class);
+
+    private static LocalFactory instance;
+
+    private final Local local = new Local();
+    private final String localIdentifier = RandomStringUtils.randomAlphabetic(8);
+
+    private static class Closer extends Thread{
+        private final Local LOCAL;
+
+        public Closer(Local local){
+            this.LOCAL = local;
+        }
+        @Override
+        public void run() {
+            try {
+                if (LOCAL.isRunning()) {
+                    LOCAL.stop();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private LocalFactory(Map<String, String> args) {
+        try {
+            args.put("localIdentifier", localIdentifier);
+            local.start(args);
+            LOGGER.debug("Started BrowserStack Local with identifier {}.", localIdentifier);
+        } catch (Exception e) {
+            LOGGER.error("Initialization BrowserStack Local with identifier {} failed.", localIdentifier);
+        }
+    }
+
+    public static LocalFactory createInstance(Map<String, String> args) {
+        if (instance == null) {
+            synchronized (LocalFactory.class) {
+                if (instance == null) {
+                    instance = new LocalFactory(args);
+                    Runtime.getRuntime().addShutdownHook(new Closer(instance.local));
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static LocalFactory getInstance() {
+        return instance;
+    }
+
+    public String getLocalIdentifier() {
+        return instance.localIdentifier;
+    }
+}
