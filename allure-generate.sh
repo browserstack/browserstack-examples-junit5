f35ca61b1f57f992f9047ecb9092242c4ae1b065
raveerocks <raveendra.tudangil@gmail.com>
2021-07-31 00:06:54 +0530

    CI Check fixed  :white_check_mark:

--- allure-generate.sh
+++ allure-generate.sh
@@ -1,10 +1,6 @@
-#copy from allure report to allure result
-
-
-if [ !($CI) ]
+if [ "$CI" != true ] ; then
 echo 'Generating Allure Reports'
 mkdir -p allure-results/history
 cp allure-report/history/* allure-results/history
 allure generate --clean allure-results
-allure serve allure-results
 fi
