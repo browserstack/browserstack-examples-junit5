8c5ff905eddb3afd536a40a41a19bc72b4dd78bf
raveerocks <raveendra.tudangil@gmail.com>
2021-07-30 15:22:21 +0530

    Jenkins Allure Fix :page_facing_up:

--- allure-generate.sh
+++ allure-generate.sh
@@ -1,7 +1,7 @@
 #copy from allure report to allure result
 
 
-if [ $CI ]
+if [ !($CI) ]
 echo 'Generating Allure Reports'
 mkdir -p allure-results/history
 cp allure-report/history/* allure-results/history
