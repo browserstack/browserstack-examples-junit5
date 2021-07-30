af43d6c4ea1b0520039f7b1eba902bf4606eae96
raveerocks <raveendra.tudangil@gmail.com>
2021-07-31 00:09:00 +0530

    CI Check fixed  :white_check_mark:

--- allure-generate.bat
+++
@@ -1,6 +0,0 @@
-if %CI% != true (
-    echo 'Generating Allure Reports'
-    if not exist allure-results/history allure-results/history
-    xcopy allure-report/history allure-results/history
-    allure generate --clean allure-results
-)
